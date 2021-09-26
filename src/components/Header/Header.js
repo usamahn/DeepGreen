2b87ecd9d43316151e04d771a888c1fd9868caa0
usamahn <oussama.henia@outlook.fr>
2021-09-26 12:03:40 +0100

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,296 @@
+import { connect } from "react-redux";
+import React from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router";
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Input,
+  UncontrolledAlert,
+  Dropdown,
+  Collapse,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Badge,
+  ButtonGroup,
+  Button,
+  Form,
+  FormGroup,
+} from "reactstrap";
+import Notifications from "../Notifications";
+import PowerIcon from "../Icons/HeaderIcons/PowerIcon";
+import BellIcon from "../Icons/HeaderIcons/BellIcon";
+import SettingsIcon from "../Icons/HeaderIcons/SettingsIcon";
+import MessageIcon from "../Icons/HeaderIcons/MessageIcon";
+import BurgerIcon from "../Icons/HeaderIcons/BurgerIcon";
+import SearchIcon from "../Icons/HeaderIcons/SearchIcon";
+import ArrowIcon from "../Icons/HeaderIcons/ArrowIcon";
+
+
+import { logoutUser } from "../../actions/user";
+import {
+  openSidebar,
+  closeSidebar,
+  changeSidebarPosition,
+  changeSidebarVisibility,
+} from "../../actions/navigation";
+
+import sender1 from "../../assets/people/a1.jpg";
+import sender2 from "../../assets/people/a5.jpg";
+import sender3 from "../../assets/people/a4.jpg";
+
+import avatar from "../../assets/people/a7.jpg";
+
+import s from "./Header.module.scss";
+import "animate.css";
+
+class Header extends React.Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    sidebarPosition: PropTypes.string.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.doLogout = this.doLogout.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+    this.toggleMessagesDropdown = this.toggleMessagesDropdown.bind(this);
+    this.toggleSupportDropdown = this.toggleSupportDropdown.bind(this);
+    this.toggleSettingsDropdown = this.toggleSettingsDropdown.bind(this);
+    this.toggleAccountDropdown = this.toggleAccountDropdown.bind(this);
+    this.toggleSidebar = this.toggleSidebar.bind(this);
+    this.toggleSearchOpen = this.toggleSearchOpen.bind(this);
+
+    this.state = {
+      visible: true,
+      messagesOpen: false,
+      supportOpen: false,
+      settingsOpen: false,
+      searchFocused: false,
+      searchOpen: false,
+      notificationsOpen: false,
+    };
+  }
+
+  toggleNotifications = () => {
+    this.setState({
+      notificationsOpen: !this.state.notificationsOpen,
+    });
+  };
+
+  onDismiss() {
+    this.setState({ visible: false });
+  }
+
+  doLogout() {
+    this.props.dispatch(logoutUser());
+  }
+
+  toggleMessagesDropdown() {
+    this.setState({
+      messagesOpen: !this.state.messagesOpen,
+    });
+  }
+
+  toggleSupportDropdown() {
+    this.setState({
+      supportOpen: !this.state.supportOpen,
+    });
+  }
+
+  toggleSettingsDropdown() {
+    this.setState({
+      settingsOpen: !this.state.settingsOpen,
+    });
+  }
+
+  toggleAccountDropdown() {
+    this.setState({
+      accountOpen: !this.state.accountOpen,
+    });
+  }
+
+  toggleSearchOpen() {
+    this.setState({
+      searchOpen: !this.state.searchOpen,
+    });
+  }
+
+  toggleSidebar() {
+    this.props.isSidebarOpened
+      ? this.props.dispatch(closeSidebar())
+      : this.props.dispatch(openSidebar());
+  }
+
+  moveSidebar(position) {
+    this.props.dispatch(changeSidebarPosition(position));
+  }
+
+  toggleVisibilitySidebar(visibility) {
+    this.props.dispatch(changeSidebarVisibility(visibility));
+  }
+
+  render() {
+    return (
+      <Navbar className={`d-print-none `}>
+        <div className={s.burger}>
+          <NavLink
+              onClick={this.toggleSidebar}
+              className={`d-md-none ${s.navItem} text-white`}
+              href="#"
+            >
+              <BurgerIcon className={s.headerIcon} />
+            </NavLink>
+        </div>
+        <div className={`d-print-none ${s.root}`}>
+
+          <Collapse
+            className={`${s.searchCollapse} ml-lg-0 mr-md-3`}
+            isOpen={this.state.searchOpen}
+          >
+          </Collapse>
+
+          <Nav className="ml-md-0">
+
+
+            
+            <NavItem className={`${s.divider} d-none d-sm-block`} />
+            <Dropdown
+              className="d-none d-sm-block"
+              nav
+              isOpen={this.state.settingsOpen}
+              toggle={this.toggleSettingsDropdown}
+            >
+              <DropdownToggle nav className={`${s.navItem} text-white`}>
+                <SettingsIcon addId='header-settings' className={s.headerIcon} />
+              </DropdownToggle>
+              <DropdownMenu className={`${s.dropdownMenu} ${s.settings}`}>
+                <h6>Sidebar on the</h6>
+                <ButtonGroup size="sm">
+                  <Button
+                    color="primary"
+                    onClick={() => this.moveSidebar("left")}
+                    className={
+                      this.props.sidebarPosition === "left" ? "active" : ""
+                    }
+                  >
+                    Left
+                  </Button>
+                  <Button
+                    color="primary"
+                    onClick={() => this.moveSidebar("right")}
+                    className={
+                      this.props.sidebarPosition === "right" ? "active" : ""
+                    }
+                  >
+                    Right
+                  </Button>
+                </ButtonGroup>
+                <h6 className="mt-2">Sidebar</h6>
+                <ButtonGroup size="sm">
+                  <Button
+                    color="primary"
+                    onClick={() => this.toggleVisibilitySidebar("show")}
+                    className={
+                      this.props.sidebarVisibility === "show" ? "active" : ""
+                    }
+                  >
+                    Show
+                  </Button>
+                  <Button
+                    color="primary"
+                    onClick={() => this.toggleVisibilitySidebar("hide")}
+                    className={
+                      this.props.sidebarVisibility === "hide" ? "active" : ""
+                    }
+                  >
+                    Hide
+                  </Button>
+                </ButtonGroup>
+              </DropdownMenu>
+            </Dropdown>
+            <Dropdown
+              className="d-none d-sm-block"
+              nav
+              isOpen={this.state.supportOpen}
+              toggle={this.toggleSupportDropdown}
+            >
+              <DropdownToggle nav className={`${s.navItem} text-white`}>
+                <BellIcon className={s.headerIcon} />
+                <div className={s.count}></div>
+              </DropdownToggle>
+              <DropdownMenu right className={`${s.dropdownMenu} ${s.support}`}>
+                <DropdownItem>
+                  <Badge color="danger">
+                    <i className="fa fa-bell-o" />
+                  </Badge>
+                  <div className={s.details}>Check out this awesome ticket</div>
+                </DropdownItem>
+                <DropdownItem>
+                  <Badge color="warning">
+                    <i className="fa fa-question-circle" />
+                  </Badge>
+                  <div className={s.details}>What is the best way to get ...</div>
+                </DropdownItem>
+                <DropdownItem>
+                  <Badge color="success">
+                    <i className="fa fa-info-circle" />
+                  </Badge>
+                  <div className={s.details}>
+                    This is just a simple notification
+                  </div>
+                </DropdownItem>
+                <DropdownItem>
+                  <Badge color="info">
+                    <i className="fa fa-plus" />
+                  </Badge>
+                  <div className={s.details}>12 new orders has arrived today</div>
+                </DropdownItem>
+                <DropdownItem>
+                  <Badge color="danger">
+                    <i className="fa fa-tag" />
+                  </Badge>
+                  <div className={s.details}>
+                    One more thing that just happened
+                  </div>
+                </DropdownItem>
+                <DropdownItem>
+                  {/* eslint-disable-next-line */}
+                  <a href="#" className="text-white">
+                    See all tickets <ArrowIcon className={s.headerIcon} maskName="bellArrow" />
+                  </a>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+            <NavItem>
+              <NavLink
+                onClick={this.doLogout}
+                className={`${s.navItem} text-white`}
+                href="#"
+              >
+                <PowerIcon className={s.headerIcon} />
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </div>
+      </Navbar>
+    );
+  }
+}
+
+function mapStateToProps(store) {
+  return {
+    isSidebarOpened: store.navigation.sidebarOpened,
+    sidebarVisibility: store.navigation.sidebarVisibility,
+    sidebarPosition: store.navigation.sidebarPosition,
+  };
+}
+
+export default withRouter(connect(mapStateToProps)(Header));
---
+++ src/pages/components/MainCharts/MainCharts.js
@@ -0,0 +1,234 @@
+import React from "react";
+
+import { Row, Col } from "reactstrap";
+
+import Widget from "../../../components/Widget/Widget";
+import ApexChart from "react-apexcharts";
+
+import s from "./Charts.module.scss";
+import { chartData, liveChart,liveChart2, liveChartInterval } from "./mock";
+import Sparklines from "../../../components/Sparklines/Sparklines";
+
+import ReactEchartsCore from "echarts-for-react/lib/core";
+
+import echarts from "echarts/lib/echarts";
+
+import "echarts/lib/chart/line";
+import "echarts/lib/chart/pie";
+import "echarts/lib/chart/themeRiver";
+import "echarts/lib/component/tooltip";
+import "echarts/lib/component/legend";
+
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import exporting from "highcharts/modules/exporting";
+import exportData from "highcharts/modules/export-data";
+
+import { Container, Alert, Button, FormGroup, Label, InputGroup, InputGroupAddon, Input, InputGroupText } from 'reactstrap';
+
+
+exporting(Highcharts);
+exportData(Highcharts);
+
+class MainCharts extends React.Component {
+  state = {
+    cd: chartData,
+    ld: liveChart,
+    ld2: liveChart2,
+    initEchartsOptions: {
+      renderer: "canvas",
+    },
+    sparklineData: {
+      series: [{ data: [1, 7, 3, 5, 7, 8] }],
+      options1: {
+        colors: ["#db2a34"],
+        plotOptions: {
+          bar: {
+            columnWidth: "50%",
+          },
+        },
+      },
+      options2: {
+        colors: ["#2477ff"],
+        plotOptions: {
+          bar: {
+            columnWidth: "50%",
+          },
+        },
+      },
+    },
+  };
+
+  componentWillUnmount() {
+    clearInterval(liveChartInterval);
+  }
+
+  render() {
+    const { cd, ld,ld2, initEchartsOptions, sparklineData } = this.state;
+    return (
+      <div className={s.root}>
+        <h1 className="page-title">
+          <span className="fw-semi-bold">Dashboard</span>
+        </h1>
+        <div>
+          <Row>
+          <Col lg={12} xs={12}>
+                  <Widget
+                    title={
+                      <h5>
+                        
+                        <span className="fw-semi-bold">CO2 Emissions</span>
+                      </h5>
+                    }
+                    close
+                    collapse
+                  >
+                    <HighchartsReact options={ld} />
+                  </Widget>
+                </Col>
+          </Row>
+          <Row>
+          <Col lg={12} xs={12}>
+                  <Widget
+                    title={
+                      <h5>
+                        
+                        <span className="fw-semi-bold">CH4 Emissions</span>
+                      </h5>
+                    }
+                    close
+                    collapse
+                  >
+                    <HighchartsReact options={ld2} />
+                  </Widget>
+                </Col>
+          </Row>
+          <Row>
+          <Col lg={12} xs={12}>
+                  <Widget
+                    title={
+                      <h5>
+                        
+                        <span className="fw-semi-bold">N2O Emissions</span>
+                      </h5>
+                    }
+                    close
+                    collapse
+                  >
+                    <HighchartsReact options={liveChart} />
+                  </Widget>
+                </Col>
+          </Row>
+
+          <Row>
+          <Col lg={4} xs={12}>
+              <Widget
+                title={
+                  <h5>
+                    <span className="fw-semi-bold">Electricity consumption</span>
+                  </h5>
+                }
+                close
+                collapse
+              >
+                <ReactEchartsCore
+                  echarts={echarts}
+                  option={cd.echarts.line}
+                  opts={initEchartsOptions}
+                  style={{ height: "365px" }}
+                />
+              </Widget>
+            </Col>
+            <Col lg={4} xs={12}>
+              <Widget
+                title={
+                  <h5>
+                    <span className="fw-semi-bold">Oil consumption</span>
+                  </h5>
+                }
+                close
+                collapse
+              >
+                <ReactEchartsCore
+                  echarts={echarts}
+                  option={cd.echarts.line2}
+                  opts={initEchartsOptions}
+                  style={{ height: "365px" }}
+                />
+              </Widget>
+            </Col>
+            <Col lg={4} xs={12}>
+              <Widget
+                title={
+                  <h5>
+                    <span className="fw-semi-bold">Water consumption</span>
+                  </h5>
+                }
+                close
+                collapse
+              >
+                <ReactEchartsCore
+                  echarts={echarts}
+                  option={cd.echarts.line3}
+                  opts={initEchartsOptions}
+                  style={{ height: "365px" }}
+                />
+              </Widget>
+            </Col>
+          </Row>
+          <Row>
+          <form onSubmit={this.doLogin}>
+                            {
+                                this.props.errorMessage && (
+                                    <Alert className="alert-sm widget-middle-overflow rounded-0" color="danger">
+                                        {this.props.errorMessage}
+                                    </Alert>
+                                )
+                            }
+                            <FormGroup className="mt">
+                              <Row>
+                                <Col>
+                                <Input id="email" className="input-transparent pl-3"  type="file" required name="email" placeholder="File"/>
+  
+                                </Col>
+                                <Col>
+                                <Button type="submit" color="success" className="auth-btn"
+                                        size="sm" style={{color: '#fff'}}>
+                                      Submit
+                                    </Button>
+                                </Col>
+                              </Row>
+                                    
+                            </FormGroup>
+                                
+
+          </form>
+          </Row>
+          <Row>
+          <Col lg={12 } xs={12}>
+              <Widget
+                title={
+                  <h5>
+                    <span className="fw-semi-bold">Historical CO2 Data</span>
+                  </h5>
+                }
+                close
+                collapse
+              >
+                <ReactEchartsCore
+                  echarts={echarts}
+                  option={cd.echarts.line}
+                  opts={initEchartsOptions}
+                  style={{ height: "365px" }}
+                />
+              </Widget>
+            </Col>
+          </Row>
+         
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainCharts;
